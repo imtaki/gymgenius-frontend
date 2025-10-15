0d4a36d640522533420ef2612357e8c5ea062c5b
Dominik Takáč <161586687+imtaki@users.noreply.github.com>
2025-10-15 21:18:33 +0200

    refactor(helper): added helper function

---
+++ frontend/src/lib/getCookie.ts
@@ -0,0 +1,6 @@
+
+export default function getCookie(name: string): string | null {
+  if (typeof document === "undefined") return null;
+  const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
+  return match ? decodeURIComponent(match[2]) : null;
+}
