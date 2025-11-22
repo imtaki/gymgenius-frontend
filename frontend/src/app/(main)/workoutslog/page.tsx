e3df6d6f83a64ecc8de5ffb288bf711e505fbfbd
Dominik Takáč <161586687+imtaki@users.noreply.github.com>
2025-11-22 01:14:59 +0100

    feature(meals): added meal modal to add meals

---
+++ frontend/src/app/(main)/workoutslog/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="min-h-screen flex justify-center">
+            <h1 className="text-3xl font-bold">Workouts Log Page</h1>
+        </div>
+    );
+};
